03a71c00293f954e307c6b7924bfe4023872259a
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-08 13:20:09 +0100

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProShopPlus.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Telephone Number Required")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Phone]
+        public string? AltPhoneNumber { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
