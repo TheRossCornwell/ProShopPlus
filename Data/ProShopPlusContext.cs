2d154fb483f5210b2f0818d3b3346256f6ce5822
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-08 13:23:59 +0100

    Created CRUD for contacts & Added Database

---
+++ Data/ProShopPlusContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProShopPlus.Models;
+
+    public class ProShopPlusContext : DbContext
+    {
+        public ProShopPlusContext (DbContextOptions<ProShopPlusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProShopPlus.Models.Contact> Contact { get; set; } = default!;
+    }
---
+++ Pages/Contacts/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProShopPlus.Models;
+
+namespace ProShopPlus.Pages.Contacts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProShopPlusContext _context;
+
+        public IndexModel(ProShopPlusContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Contact != null)
+            {
+                Contact = await _context.Contact.ToListAsync();
+            }
+        }
+    }
+}
