368ee0d17eaa53015f8cc1ec916c451907a4265d
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-11 11:25:17 +0100

    Added Repair Table Migration

---
+++ Migrations/20230511100936_RepairTableAddition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProShopPlus.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepairTableAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Repair",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContactID = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    TargetDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Component = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Cost = table.Column<double>(type: "REAL", nullable: true),
+                    Paid = table.Column<string>(type: "TEXT", nullable: false),
+                    Balance = table.Column<double>(type: "REAL", nullable: true),
+                    Progress = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repair", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Repair_Contact_ContactID",
+                        column: x => x.ContactID,
+                        principalTable: "Contact",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repair_ContactID",
+                table: "Repair",
+                column: "ContactID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Repair");
+        }
+    }
+}
