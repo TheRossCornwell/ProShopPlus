f1f125846300fefed8202feb9f4ac72ec1b8b641
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-11 23:27:45 +0100

    Added a bunch of Javascript functions

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProShopPlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ProShopPlus.Data.ProShopPlusContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("GetContactNames")]
+        public IActionResult GetContactNames(string term)
+        {
+            var a = _context.Contact.Where(c => c.Name.StartsWith(term)).Select(a => new { label = a.Name, id = a.ID });
+
+            if (a is null)
+            {
+                return NotFound(term);
+            }
+            return Ok(a);
+        }
+
+
+    }
+}
