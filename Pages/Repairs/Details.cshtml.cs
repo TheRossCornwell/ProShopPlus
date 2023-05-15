6131bc5c3dca7786af823d1ee237f07b95697e87
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-16 00:53:00 +0100

    Repair Detail Page design created

--- Pages/Repairs/Details.cshtml.cs
+++ Pages/Repairs/Details.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProShopPlus.Data;
 using ProShopPlus.Models;
@@ -19,22 +20,27 @@ namespace ProShopPlus.Pages.Repairs
             _context = context;
         }
 
-      public Repair Repair { get; set; } = default!; 
+      public Repair Repair { get; set; } = default!;
+        public Contact Contact { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+            ViewData["ContactID"] = new SelectList(_context.Contact, "ID", "Name");
             if (id == null || _context.Repair == null)
             {
                 return NotFound();
             }
-
+            
             var repair = await _context.Repair.FirstOrDefaultAsync(m => m.ID == id);
+            
             if (repair == null)
             {
                 return NotFound();
             }
             else 
             {
+                var contact = await _context.Contact.FirstOrDefaultAsync(m => m.ID == repair.ContactID);
+                Contact = contact;
                 Repair = repair;
             }
             return Page();
