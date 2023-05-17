c3e0e41a2aa92145aa52b2c8c41f6420b81c85d0
TheRossCornwell <48067724+TheRossCornwell@users.noreply.github.com>
2023-05-17 17:58:32 +0100

    Order Index Page Progression

--- Pages/Orders/Index.cshtml.cs
+++ Pages/Orders/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProShopPlus.Data;
 using ProShopPlus.Models;
@@ -21,13 +22,39 @@ namespace ProShopPlus.Pages.Orders
 
         public IList<Order> Order { get;set; } = default!;
 
+        public List<Order> OrderList { get;set; } = default!;
+        public List<Contact> ContactList { get; set; } = default!;
+
         public async Task OnGetAsync()
         {
-            if (_context.Order != null)
+            var orders = from c in _context.Order
+                          select c;
+
+
+            orders = orders.Where(c => !HideList.Contains(c.Progress));
+            orders = orders.Where(c => c.ID != 0);
+
+            var contacts = from c in _context.Contact
+                           select c;
+            ContactList = await contacts.ToListAsync();
+
+            //Search function
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Order = await _context.Order
-                .Include(o => o.Contact).ToListAsync();
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                orders = orders.Where(c => c.Contact.Name.ToLower().Contains(SearchString.ToLower()));
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             }
+
+            ViewData["ContactID"] = new SelectList(_context.Contact, "ID", "Name");
+            OrderList = await orders.ToListAsync();
         }
+
+        // Search Properites
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; } // Search Function variable
+
+        [BindProperty(SupportsGet = true)]
+        public List<string> HideList { get; set; }
     }
 }
